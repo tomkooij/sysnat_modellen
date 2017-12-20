8ae52944cfc10cefc1cab644508e3ba8e59c046c
Tom Kooij <tomkooij@tomkooij.nl>
2017-12-20 21:08:13 +0100

    add sysnat models

--- modellen/models.js
+++ modellen/models.js
@@ -1,13 +1,5 @@
 // definieer hier de modellen
 
 var model_index = [
-    {url:'modellen/examenvwo2005.xml', title: 'Examen vwo 2005-I Champignon'},
-    {url:'modellen/examenvwo2006.xml', title: 'Examen vwo 2006-II Sojoez'},
-    {url:'modellen/examenvwo2013.xml', title: 'Examen vwo 2013-II Trekkertrek'},
-    {url:'modellen/examenvwo2014.xml', title: 'Examen vwo 2014-II Kogelstoten'},
-    {url:'modellen/examenvwo2015.xml', title: 'Examen vwo 2015-I Sprong bij Volleybal'},
-    {url:'modellen/examenvwo2016.xml', title: 'Examen vwo 2016-I Ruimtelift'},
-    {url:'modellen/examenvwo2016-2.xml', title: 'Examen vwo 2016-II Jupiter Fly-by'},
-    {url:'modellen/examenvwo2017.xml', title: 'Examen vwo 2017-I Cessna'},
-    {url:'modellen/examenvwo2017-2.xml', title: 'Examen vwo 2017-II X-stream'},
+    {url:'modellen/Model 1 Constante snelheid.xml', title: 'Model 1'},
 ];
