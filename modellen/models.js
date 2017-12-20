8397d3859a506192d0d8ffe9f7bd80729c8b4068
Tom Kooij <tomkooij@tomkooij.nl>
2017-12-20 21:58:44 +0100

    Model 13

--- modellen/models.js
+++ modellen/models.js
@@ -9,6 +9,7 @@ var model_index = [
     {url:'modellen/Model W1 incompleet.xml', title: 'Model W1 (incompleet)'},
     {url:'modellen/Model 5 optrekken auto.xml', title: 'Model 5 optrekken auto'},
     {url:'modellen/parachutesprong.xml', title: 'Model 9 Parachutesprong'},
+    {url:'modellen/Model 13 Raket.xml', title: 'Model 13 Raket. Verticale lancering'},
     {url:'modellen/Model 15 Basketbal.xml', title: 'Model 15 Basketbal (incompleet)'},
     {url:'modellen/Model 17 Satelliet.xml', title: 'Model 17 Satelliet'},
 ];
