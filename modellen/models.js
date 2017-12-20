df2132ef990477a8b5bbfcebf03148d17cc68eef
Tom Kooij <tomkooij@tomkooij.nl>
2017-12-20 22:14:36 +0100

    Fix typo

--- modellen/models.js
+++ modellen/models.js
@@ -3,8 +3,8 @@
 var model_index = [
     {url:'modellen/Model 1 Constante snelheid.xml', title: 'Model 1 Constante snelheid'},
     {url:'modellen/Model 2 Eenparig versnelde beweging.xml', title: 'Model 2 Eenparig versnelde beweging'},
-    {url:'modellen/Model 3 Rente incompleet.xml', title: 'Model Model 3 Rente (incompleet)'},
-    {url:'modellen/Model 3 Rente compleet.xml', title: 'Model Model 3 Rente (compleet)'},
+    {url:'modellen/Model 3 Rente incompleet.xml', title: 'Model 3 Rente (incompleet)'},
+    {url:'modellen/Model 3 Rente compleet.xml', title: 'Model 3 Rente (compleet)'},
     {url:'modellen/Model W1 compleet.xml', title: 'Model W1 (compleet)'},
     {url:'modellen/Model W1 incompleet.xml', title: 'Model W1 (incompleet)'},
     {url:'modellen/Model 5.xml', title: 'Model 5 optrekken auto'},
