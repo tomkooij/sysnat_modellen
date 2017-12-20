0317eb37fd2ae683987e8012e8e20bacbf3446dd
Tom Kooij <tomkooij@tomkooij.nl>
2017-12-20 21:23:44 +0100

    Update models.js index

--- modellen/models.js
+++ modellen/models.js
@@ -1,5 +1,13 @@
 // definieer hier de modellen
 
 var model_index = [
-    {url:'modellen/Model 1 Constante snelheid.xml', title: 'Model 1'},
+    {url:'modellen/Model 1 Constante snelheid.xml', title: 'Model 1 Constante snelheid'},
+    {url:'modellen/Model 2 Eenparig versnelde beweging.xml', title: 'Model 2 Eenparig versnelde beweging'},
+    {url:'modellen/Model 3 Rente incompleet.xml', title: 'Model Model 3 Rente (incompleet)'},
+    {url:'modellen/Model 3 Rente compleet.xml', title: 'Model Model 3 Rente (compleet)'},
+    {url:'modellen/Model W1 (compleet).xml', title: 'Model W1 (compleet)'},
+    {url:'modellen/Model W1 (incompleet).xml', title: 'Model W1 (incompleet)'},
+    {url:'modellen/Model 5 optrekken auto.xml', title: 'Model 5 optrekken auto'},
+    {url:'modellen/parachutesprong.xml', title: 'Model 9 Parachutesprong'},
+    {url:'modellen/satelliet.xml', title: 'Model 15 Satelliet'},
 ];
