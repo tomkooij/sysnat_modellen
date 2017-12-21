431b793df0566bada6396b145cd6569371a20621
Tom Kooij <tomkooij@tomkooij.nl>
2017-12-21 11:49:08 +0100

    Add model W6 and model W3

--- modellen/models.js
+++ modellen/models.js
@@ -4,13 +4,13 @@ var model_index = [
 // par 5.1
   {url:'modellen/Model 3 Rente incompleet.xml', title: 'Opgave 1: Model 3 Rente (incompleet)'},
   {url:'modellen/Model 3 Rente compleet.xml', title: 'Opgave 1: Model 3 Rente (compleet)'},
-  {url:'modellen/Model W1 compleet.xml', title: 'Opgave 2: Model W1 (compleet)'},
   {url:'modellen/Model W1 incompleet.xml', title: 'Opgave 2: Model W1 (incompleet)'},
+  {url:'modellen/Model W1 compleet.xml', title: 'Opgave 2: Model W1 (compleet)'},
   {url:'modellen/Model 2 Eenparig versnelde beweging.xml', title:  'Opgave 3: Model 2 Eenparig versnelde beweging'},
 // par 5.2
   {url:'modellen/Model 6 Optrekken met luchtweerstand.xml', title: 'Opgave 4: Model 6 Optrekken met luchtweerstand'},
   {url:'modellen/Model 9 Parachutesprong.xml', title: 'Opgave 5: Model 9 Parachutesprong'},
-  {url:'modellen/Model W3.xml', title: 'Opgave 5: Model W3 ONTBREEKT'},
+  {url:'modellen/Model W3 Parachutesprong incompleet.xml', title: 'Opgave 5: Model W3 Parachutesprong incompleet'},
   {url:'modellen/Model 10 Harmonische trilling.xml', title: 'Opgave 6: Model 10 Harmonische trilling'},
 // par 5.3
   {url:'modellen/Model 11 Tennisbal.xml', title: 'Opgave 7: Model 11 Tennisbal'},
@@ -18,7 +18,8 @@ var model_index = [
   {url:'modellen/Model 13 Raket.xml', title: 'Opgave 9: Model 13 Raket. Verticale lancering'},
   {url:'modellen/Model 16 Raket incompleet.xml', title: 'Opgave 10: Model 16 Raket (incompleet).'},
 // par 5.4
-  {url:'modellen/Model 17 Satelliet.xml', title: 'Opgave 11-13: Model 17 Satelliet'},
+  {url:'modellen/Model 17 Satelliet.xml', title: 'Opgave 11-12: Model 17 Satelliet'},
+  {url:'modellen/Model W6 Coulomb.xml', title: 'Opgave 3: Model W6 Coulomb (incompleet)'},
 // rest
   {url:'', title: '----EXTRA----'},
   {url:'modellen/Model 1 Constante snelheid.xml', title: 'Model 1 Constante snelheid'},
